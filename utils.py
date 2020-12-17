775782465de94931123efe201600bcff90c58d53
Manas Macherla <rmacher@g.clemson.edu>
2020-12-16 23:53:28 -0500

    wrote function for calculating iou b/w 2 bboxs

--- utils.py
+++ utils.py
@@ -16,6 +16,31 @@ def unique(tensor):
     tensor_res.copy_(unique_tensor)
     return tensor_res
 
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes 
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    
+    #Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+ 
+    #Union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    
+    return iou
+
 def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
 
     batch_size = prediction.size(0)
@@ -114,3 +139,20 @@ def write_results(prediction, confidence, num_classes, nms_conf):
             conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1] #outputs two tensors (values and indices) ind = slicing 2nd array
             image_pred_class = image_pred_class[conf_sort_index] # rearraging the prediction tensor in a descending manner 
             idx = image_pred_class.size(0)   #Number of detections
+
+            for i in range(idx):
+                #get the IoUs of all boxes that come after the one we are looking at in the loop
+                try:
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:]) #returns a tensor containing ious with the ith bbox
+                except ValueError:
+                    break #if the returned image_pred_class[i+1:] tensor is an empty tensor, raises a value error
+                except IndexError:
+                    break #this might occur since we are eliminating bboxes with iou > threshold, indexing a value that is out of bounds 
+
+                #Zero out all the detections that have IoU > treshhold
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i+1:] *= iou_mask       
+
+                #Remove the non-zero entries
+                non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
